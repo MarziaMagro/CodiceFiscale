e2d8c9d10e67ad09c27073d017e90a4e26661be6
marzia.magro <mrz.magro@gmail.com>
2025-06-06 18:37:55 +0200

    Initial commit - Spring Boot CF API

---
+++ src/main/java/com/example/cf_api/CfApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cf_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfApiApplication.class, args);
+	}
+
+}
